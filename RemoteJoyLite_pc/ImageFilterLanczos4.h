d58fca7b91c500356f108451b22efd2f9afabd6b
DOODLEDO3946 <64034796+DOODLEDO3946@users.noreply.github.com>
2026-06-02 14:22:09 +0100

    Update ImageFilterLanczos4.h

--- RemoteJoyLite_pc/ImageFilterLanczos4.h
+++ RemoteJoyLite_pc/ImageFilterLanczos4.h
@@ -2,6 +2,7 @@
 #define IMAGE_FILTER_LANCZOS4_H
 
 // #include <atlbase.h>
+#include <wrl/client.h>
 #include "ImageFilterPixelShaderBase.h"
 
 struct IDirect3DPixelShader9;
@@ -10,12 +11,12 @@ class ImageFilterLanczos4 : public ImageFilterPixelShaderBase {
 public:
 	ImageFilterLanczos4();
 	virtual ~ImageFilterLanczos4();
-	virtual const CComPtr<IDirect3DPixelShader9>& getPixelShaderCache();
-	virtual void setPixelShaderCache(const CComPtr<IDirect3DPixelShader9>& pixelShader);
+	virtual const Microsoft::WRL::ComPtr<IDirect3DPixelShader9>& getPixelShaderCache();
+	virtual void setPixelShaderCache(const Microsoft::WRL::ComPtr<IDirect3DPixelShader9>& pixelShader);
 	static void release();
 
 private:
-	static CComPtr<IDirect3DPixelShader9> pixelShaderCache;
+	static Microsoft::WRL::ComPtr<IDirect3DPixelShader9> pixelShaderCache;
 };
 
 #endif
