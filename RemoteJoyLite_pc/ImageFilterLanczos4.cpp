71f3a9a2e75982ee62f955ba60aa4cfeddf07b32
DOODLEDO3946 <64034796+DOODLEDO3946@users.noreply.github.com>
2026-06-02 14:29:49 +0100

    Update ImageFilterLanczos4.cpp

--- RemoteJoyLite_pc/ImageFilterLanczos4.cpp
+++ RemoteJoyLite_pc/ImageFilterLanczos4.cpp
@@ -1,12 +1,12 @@
 #include <windows.h>
-// #include <atlbase.h>
+#include <wrl/client.h>
 #include <d3d9.h>
 #include <d3dx9.h>
 #include <dxerr.h>
 #include "ImageFilterLanczos4.h"
 
 extern const char* PIXEL_SHADER_LANCZOS4;
-CComPtr<IDirect3DPixelShader9> ImageFilterLanczos4::pixelShaderCache;
+Microsoft::WRL::ComPtr<IDirect3DPixelShader9> ImageFilterLanczos4::pixelShaderCache;
 
 ImageFilterLanczos4::ImageFilterLanczos4() : ImageFilterPixelShaderBase(PIXEL_SHADER_LANCZOS4) {
 }
@@ -14,11 +14,11 @@ ImageFilterLanczos4::ImageFilterLanczos4() : ImageFilterPixelShaderBase(PIXEL_SH
 ImageFilterLanczos4::~ImageFilterLanczos4() {
 }
 
-const CComPtr<IDirect3DPixelShader9>& ImageFilterLanczos4::getPixelShaderCache() {
+const Microsoft::WRL::ComPtr<IDirect3DPixelShader9>& ImageFilterLanczos4::getPixelShaderCache() {
 	return pixelShaderCache;
 }
 
-void ImageFilterLanczos4::setPixelShaderCache(const CComPtr<IDirect3DPixelShader9>& pixelShader) {
+void ImageFilterLanczos4::setPixelShaderCache(const Microsoft::WRL::ComPtr<IDirect3DPixelShader9>& pixelShader) {
 	pixelShaderCache = pixelShader;
 }
 
